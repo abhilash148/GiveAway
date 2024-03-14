6002077aca045427c7d1186c13bff4bb3003e1a7
abhilash148 <saiabhilash148@gmail.com>
2024-03-14 18:19:15 -0500

    03-14-2024: Login page created

--- App.tsx
+++ App.tsx
@@ -5,114 +5,105 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import React, {useState} from 'react';
+import {Text, View, Image, ScrollView, TextInput, Button} from 'react-native';
+import { FlatList, StyleSheet } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+const name = "Abhilash";
+const getFullName = (
+  firstName: string,
+  secondName: string,
+  thirdName: string
+  ) => {
+    return firstName + ' ' + secondName + ' ' + thirdName;
+  }
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+type CatProps = {
+  name: string;
+};
+
+const Cat = (props: CatProps) => {
+
+  const [isHungry, setIsHungry] = useState(true);
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
+    <View>
+      <Text>I am {props.name}, and I am {isHungry ? 'hungry' : 'full'}!</Text>
+      <Button onPress={ () => {
+        setIsHungry(false);
+      }}
+      disabled={!isHungry}
+      title={isHungry ? 'Pour me some milk, please!' : 'Thank you!'}
+      />
+    </View>
+  );
+
+};
+
+const Cafe = () => {
+  return (
+    <View>
+      <Text>Welcome!</Text>
+      <Cat name='Abhilash'/>
+      <Cat name='Santosh'/>
+      <Cat name='Yashwanth'/>
+    </View>
+  );
+};
+
+const PizzaTranslator = () => {
+  const [text, setText] = useState('');
+
+  return (
+    <View style={{padding: 10}}>
+      <TextInput
+        style={{height: 40}}
+        placeholder='Type here to translate!'
+        onChangeText={newText => setText(newText)}
+        defaultValue={text}
+      />
+
+      <Text style={{padding: 10, fontSize: 42}}>
+        {text
+          .split(' ')
+          .map(word => word && 'pizza')
+          .join(' ')}
       </Text>
     </View>
   );
-}
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    paddingTop: 22,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
+  item: {
+    padding: 10,
     fontSize: 18,
-    fontWeight: '400',
+    height: 44,
   },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+}
+);
 
-export default App;
+const FlatListBasics = () => {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={[
+          {key: 'Abhilash'},
+          {key: 'Santosh'},
+          {key: 'Yashwanth'},
+          {key: 'Praveen'},
+        ]}
+        renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+      />
+
+    </View>
+  );
+};
+
+
+export default FlatListBasics;
---
+++ Login.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const Cat = () => {
+    const name = "Abhilash";
+    return <Text>Hello, I am {name}!</Text>;
+};
+
+export default Cat;

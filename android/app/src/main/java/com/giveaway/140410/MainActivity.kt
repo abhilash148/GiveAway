78244a700d346620d31faa9f120a272ac64cc865
abhilash148 <saiabhilash148@gmail.com>
2024-04-14 21:53:47 -0500

    playstore config changes

--- android/app/src/main/java/com/giveaway/140410/MainActivity.kt
+++ android/app/src/main/java/com/giveaway/140410/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.giveaway5320
+package com.giveaway.140410
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "GiveAway"
+  override fun getMainComponentName(): String = "giveaway140410"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
--- ios/giveaway140410Tests/giveaway140410Tests.m
+++ ios/giveaway140410Tests/giveaway140410Tests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface GiveAwayTests : XCTestCase
+@interface giveaway140410Tests : XCTestCase
 
 @end
 
-@implementation GiveAwayTests
+@implementation giveaway140410Tests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
